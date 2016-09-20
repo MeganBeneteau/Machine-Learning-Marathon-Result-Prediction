2065aee68d0b433d5eb4bf708aead07a145c23e1
elisabethsulmont <esulmont@gmail.com>
2016-09-19 22:07:19 -0400

    prediction percentage

--- k_fold_cross_validation.m
+++ k_fold_cross_validation.m
@@ -1,6 +1,6 @@
 %%%%%%%%%%%%DATA LOADING%%%%%%%%%%%%
-%data = load('MontrealMarathon.txt');
-X = data(:, :); %all the data
+data = load('MontrealMarathon.txt');
+Data = data(:, :); %all the data
 [n, numColumns] = size(A); %n = number of participants
 m = numColumn - 1; %nmumber of features
 
@@ -10,13 +10,14 @@ foldId = kron( 1:k, ones(1,foldSize) );
 
 for i = 1 : k
     %%% form train and test data %%%%
-    trainIdx = find( foldId == i );
-    testIdx  = find( foldId ~= i );
-    trainX = X(trainIdx, :);
-    testX = X(testIdx, :); 
+    trainId = find( foldId == i );
+    testId  = find( foldId ~= i );
+    train = Data(trainId, :);
+    test = Data(testId, :); 
     
-    w = log_reg(trainX,m); %% get weights from trainX
-    predictionError(w, testX); %% find prediction error for iteration
+    w = log_reg(train,m); %% get weights from trainX
+    predError = predictionError(w, test); %% find prediction error for iteration
+    disp(predError);
     
 end
 
