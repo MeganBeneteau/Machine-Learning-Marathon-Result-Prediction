20f51cb1f8dacff823b8603a5a2e34bcd00edae7
elisabethsulmont <esulmont@gmail.com>
2016-09-22 11:30:57 -0400

    clean up

--- logistic_function.m
+++
@@ -1,6 +0,0 @@
-function p = logistic_function(z)
-
-p= 1 / (1+(exp(-z)));
-
-end
-
--- test.m
+++
@@ -1,6 +0,0 @@
-%%%%%%%%%%%%DATA LOADING%%%%%%%%%%%%
-data = load('lis_data.csv');
-
-Data = data(2:5, [10]); %all the data
-disp(Data);
-[m,n] = size(Data); 
